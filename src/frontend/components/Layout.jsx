530bf2f1583a9347fefa3dbf2a4212a3d1276142
Luis Fernando Mendez <luisfernando.mendez5@gmail.com>
2021-04-10 12:27:29 -0400

    update

--- src/frontend/components/Layout.jsx
+++ src/frontend/components/Layout.jsx
@@ -1,13 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { connect } from 'react-redux';
-import Header from './Header';
-import Menu from './Menu';
-import Modal from '../portals/Modal';
 
-const Layout = ({ children, user }) => {
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import TextField from '@material-ui/core/TextField';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import PhoneIcon from '@material-ui/icons/Phone';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import Peer from 'simple-peer';
+import '../videollamada/App.css';
+import { useHistory } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import Modal from '../portals/Modal';
+import Menu from './Menu';
+import Header from './Header';
+import * as actionsStatus from '../actions';
+import socket from '../socket';
+
+const Layout = ({ children, user, getCall, gettingCall }) => {
 
   const [isOpenClient, setIsOpenClient] = useState(false);
-  const [isOpenConsultant, setIsOpenConsultant] = useState(false);
+
+  const history = useHistory();
+  const [me, setMe] = useState('');
+  const [stream, setStream] = useState();
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState('');
+  const [callerSignal, setCallerSignal] = useState();
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [idToCall, setIdToCall] = useState('');
+  const [name, setName] = useState('');
+  const [idRoom, setIdRoom] = useState('');
+
+  const connectionRef = useRef();
 
   const handleCloseClient = () => {
     setIsOpenClient(false);
@@ -15,6 +40,41 @@ const Layout = ({ children, user }) => {
 
   const handleCloseConsultant = () => {
     setIsOpenConsultant(false);
+  };
+
+  useEffect(() => {
+    socket.on('me', (id) => {
+      console.log(id);
+      setMe(id);
+    });
+
+    socket.on('callUser', (data) => {
+      setReceivingCall(true);
+      setCaller(data.from);
+      setName(data.name);
+      setCallerSignal(data.signal);
+      setIdRoom(uuidv4());
+
+    });
+  }, []);
+
+  const answerCall = () => {
+
+    setCallAccepted(true);
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream,
+    });
+    peer.on('signal', (data) => {
+      socket.emit('answerCall', { signal: data, to: caller, idRoom });
+    });
+
+    peer.signal(callerSignal);
+    connectionRef.current = peer;
+
+    console.log('El id en answerCall es ', idRoom);
+    history.push(`/join/${idRoom}`);
   };
 
   if (user.status && user.status.isCall) {
@@ -31,9 +91,21 @@ const Layout = ({ children, user }) => {
         <Header />
         {children}
         <Menu />
-        <Modal isOpen={isOpenConsultant} onClose={handleCloseConsultant}>
-          {/* <NotificationConsultant /> */}
-        </Modal>
+        <div>
+          {receivingCall ? (
+            <div className='caller'>
+              <Button variant='contained' color='primary' onClick={answerCall}>
+                <h1>
+                  {name}
+                  {' '}
+                  is calling...
+                </h1>
+                Answer
+              </Button>
+            </div>
+          ) : null}
+        </div>
+        {/* <NotificationConsultant /> */}
       </div>
     );
   }
@@ -64,4 +136,4 @@ const mapStateToProps = (reducers) => {
   return reducers.statusReducers;
 };
 
-export default connect(mapStateToProps, null)(Layout);
+export default connect(mapStateToProps, actionsStatus)(Layout);
