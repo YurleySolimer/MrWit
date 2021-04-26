1f98bb289ee7e01f9dba5b49674c27ae92457e23
Luis Fernando Mendez <luisfernando.mendez5@gmail.com>
2021-04-25 23:14:47 -0400

    hostname

--- src/frontendcra/src/index.js
+++ src/frontendcra/src/index.js
@@ -19,7 +19,7 @@ const store = createStore(
 
 axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://app.mrwit.co/api';
 console.log(axios.defaults.baseURL);
-console.log(window.location.href);
+console.log(window.location.hostname);
 
 ReactDOM.render(
   <Provider store={store}>
